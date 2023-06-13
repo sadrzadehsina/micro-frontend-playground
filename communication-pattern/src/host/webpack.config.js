e369ae29c70bd266db6794b3b31559410acf0bbc
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-06-13 15:39:54 +0330

    Add host app

---
+++ communication-pattern/src/host/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devtool: "source-map",
+  entry: "/src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js(x)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env"],
+              [
+                "@babel/preset-react",
+                {
+                  runtime: "automatic",
+                  development: true
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
